d9849e36b62e0f5d57d93bc6b19c483ee3a79099
Fayas Noushad <TheFayasNoushad@gmail.com>
2021-05-07 18:28:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,7 @@
 import os
 import pyrogram
 from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 FayasNoushad = Client(
     "Telegram Attach Bot",
@@ -14,5 +15,109 @@ FayasNoushad = Client(
     api_id = int(os.environ["API_ID"]),
     api_hash = os.environ["API_HASH"]
 )
+
+START_TEXT = """
+Hello {}, I am small media or file in a message attach bot. I can attach photo, video, audio etc. using their public links in a message.
+
+Made by @FayasNoushad
+"""
+HELP_TEXT = """
+<u><b>Steps :-</b></u>
+- Just send a message
+- Reply a link for attaching
+
+<u><b>Tips :-</b></u>
+• Use @FnTelegraphBot for telegraph links of photos and videos 
+• Use Telegram public channel or group message links
+• You can send any type of links for attaching
+Made by @FayasNoushad
+"""
+ABOUT_TEXT = """
+- **Bot :** `Attach Bot`
+- **Creator :** [Fayas](https://telegram.me/TheFayas)
+- **Channel :** [Fayas Noushad](https://telegram.me/FayasNoushad)
+- **Source :** [Click here](https://github.com/FayasNoushad/Attach-Bot)
+- **Language :** [Python3](https://python.org)
+- **Library :** [Pyrogram](https://pyrogram.org)
+- **Server :** [Heroku](https://heroku.com)
+"""
+START_BUTTONS = InlineKeyboardMarkup(
+        [[
+        InlineKeyboardButton('Channel', url='https://telegram.me/FayasNoushad'),
+        InlineKeyboardButton('Feedback', url='https://telegram.me/TheFayas')
+        ],[
+        InlineKeyboardButton('Help', callback_data='help'),
+        InlineKeyboardButton('About', callback_data='about'),
+        InlineKeyboardButton('Close', callback_data='close')
+        ]]
+    )
+HELP_BUTTONS = InlineKeyboardMarkup(
+        [[
+        InlineKeyboardButton('Home', callback_data='home'),
+        InlineKeyboardButton('About', callback_data='about'),
+        InlineKeyboardButton('Close', callback_data='close')
+        ]]
+    )
+ABOUT_BUTTONS = InlineKeyboardMarkup(
+        [[
+        InlineKeyboardButton('Home', callback_data='home'),
+        InlineKeyboardButton('Help', callback_data='help'),
+        InlineKeyboardButton('Close', callback_data='close')
+        ]]
+    )
+
+@FayasNoushad.on_callback_query()
+async def cb_handler(bot, update):
+    if update.data == "home":
+        await update.message.edit_text(
+            text=START_TEXT.format(update.from_user.mention),
+            reply_markup=START_BUTTONS,
+            disable_web_page_preview=True
+        )
+    elif update.data == "help":
+        await update.message.edit_text(
+            text=HELP_TEXT,
+            reply_markup=HELP_BUTTONS,
+            disable_web_page_preview=True
+        )
+    elif update.data == "about":
+        await update.message.edit_text(
+            text=ABOUT_TEXT,
+            reply_markup=ABOUT_BUTTONS,
+            disable_web_page_preview=True
+        )
+    else:
+        await update.message.delete()
+
+@FayasNoushad.on_message(filters.private & filters.command(["start"]))
+async def start(bot, update):
+    await update.reply_text(
+        text=START_TEXT.format(update.from_user.mention),
+        disable_web_page_preview=True,
+        reply_markup=START_BUTTONS
+    )
+
+@FayasNoushad.on_message(filters.private & filters.command(["help"]))
+async def help(bot, update):
+    await update.reply_text(
+        text=HELP_TEXT,
+        disable_web_page_preview=True,
+        reply_markup=HELP_BUTTONS
+    )
+
+@FayasNoushad.on_message(filters.private & filters.command(["about"]))
+async def about(bot, update):
+    await update.reply_text(
+        text=ABOUT_TEXT,
+        disable_web_page_preview=True,
+        reply_markup=ABOUT_BUTTONS
+    )
+
+@FayasNoushad.on_message(filters.text & filters.private & filters.reply & filters.regex(pattern=".*http.*"))
+async def attach(bot, update):
+    await update.reply_text(
+        text=f"[\u2063]({update.text}){update.reply_to_message.text}",
+        reply_markup=update.reply_to_message.reply_markup
+    )
 
 FayasNoushad.run()
